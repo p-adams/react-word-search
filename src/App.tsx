d81c57665c6f61e8a49331e4efdde0d30227c613
Paul Adams <ghusiyy@gmail.com>
2024-01-02 01:20:42 -0800

    render grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 
 function App() {
+  const grid = [
+    ["A", "B", "C", "E"],
+    ["S", "F", "C", "S"],
+    ["A", "D", "E", "E"],
+  ];
   return (
     <>
       <h1>React Word Search</h1>
-      <div className="card"></div>
+      <div className="card">
+        <div className="word-grid">
+          {grid.map((row, _i) =>
+            row.map((c) => <div className="cell">{c}</div>)
+          )}
+        </div>
+      </div>
     </>
   );
 }
